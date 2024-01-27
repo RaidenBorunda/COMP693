a275e9721f0dc3a96a6ba36ae4f3ffe08bd5be66
RaidenBorunda <137729767+RaidenBorunda@users.noreply.github.com>
2024-01-26 19:14:58 -0800

    Add files via upload

---
+++ module04/src/employees.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import EmployeeList from './EmployeeList.jsx'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+    <React.StrictMode>
+        <EmployeeList />
+    </React.StrictMode>, 
+    document.getElementById('content'))
